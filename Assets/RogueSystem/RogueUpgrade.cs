8327817b75b7f955487a4f7c9ab44c33712210ec
ConnorGDevelopment <60874253+ConnorGDevelopment@users.noreply.github.com>
2024-08-30 13:52:02 -0400

    Init

---
+++ Assets/RogueSystem/RogueUpgrade.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueStatBlock {
+    public int dmg;
+};
+
+[CreateAssetMenu(menuName ="Rogue/Upgrade",order =1)]
+public class RogueUpgrade : ScriptableObject
+
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
