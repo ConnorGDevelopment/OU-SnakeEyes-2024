59c0480320e9a1e65384dbbc86afadeedac08674
ConnorGDevelopment <60874253+ConnorGDevelopment@users.noreply.github.com>
2024-09-06 13:51:35 -0400

    Update RogueUpgrade.cs

--- Assets/RogueSystem/RogueUpgrade.cs
+++ Assets/RogueSystem/RogueUpgrade.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface IRogueUpgrade {
-    public bool Unique { get; }
-    public string Name { get; }
-    public int Damage { get; }
-    public int Speed { get; }
-    public int Projectiles { get; }
-    public int Spread { get; }
+    public bool Unique { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int Speed { get; set; }
+    public int Projectiles { get; set; }
+    public int Spread { get; set; }
 }
 
 [CreateAssetMenu(menuName ="RogueUpgrade",order =1)]
 public class RogueUpgrade : ScriptableObject, IRogueUpgrade
 {
-    public bool Unique;
-    public string Name;
-    public int Damage;
-    public int Speed;
-    public int Projectiles;
-    public int Spread;
+    public bool Unique { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int Speed { get; set; }
+    public int Projectiles { get; set; }
+    public int Spread { get; set; }
 
 }
