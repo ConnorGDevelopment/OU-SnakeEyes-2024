7fdbbed5030f2d02021b421f22a15a05cb0f67b8
ConnorGDevelopment <60874253+ConnorGDevelopment@users.noreply.github.com>
2024-09-09 18:06:26 -0400

    Rogue System Notating

--- Assets/RogueSystem/RogueUpgradeManager.cs
+++ Assets/RogueSystem/RogueUpgradeManager.cs
@@ -1,35 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
-public class RogueUpgrade
-{
-    // If Unique == true, duplicates of this upgrade don't stack
-    public bool Unique { get; set; }
-    public string Name { get; set; }
-    // The following are TBD, should match weapon stats
-    public int Damage { get; set; }
-    public int Speed { get; set; }
-    public int Projectiles { get; set; }
-    public int Spread { get; set; }
-    // Indexer Function
-    // Basically assigns a function to this.propertynamegoeshere, if it exists and is an integer it returns that value, otherwise it returns 0
-    // This lets us index access properties on objects of this class as if it were an array like "this[propertynamegoeshere]"
-    public int this[string key] {
-        get
-        {
-            if (this[key].GetType() == typeof(int)) {
-                return this[key];
-            } else
-            {
-                // If this weren't a getter I could use int? which basically means this value is either int or undefined
-                return 0;
-            }
-        }
-    }
-}
-
 
 public class RogueUpgradeManager : MonoBehaviour
 {
@@ -39,7 +10,7 @@ public class RogueUpgradeManager : MonoBehaviour
     {
         get {
             // Create a blank RogueUpgrade so we can assign stats
-            RogueUpgrade _summary = new();
+            RogueUpgrade _summary = ScriptableObject.CreateInstance<RogueUpgrade>();
 
             // Upgrades is our list of upgrades (duh)
             // Upgrades.GetType() returns the type of the items in the list (RogueUpgrade)
