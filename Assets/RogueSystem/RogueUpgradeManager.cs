6b075ff48c086c33b844cc737425e2c76293e724
Connor Guarino <connorgdevelopment@gmail.com>
2024-09-10 14:25:06 -0400

    Start of filtering no dupe uniques

--- Assets/RogueSystem/RogueUpgradeManager.cs
+++ Assets/RogueSystem/RogueUpgradeManager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 public class RogueUpgradeManager : MonoBehaviour
 {
@@ -8,18 +7,38 @@ public class RogueUpgradeManager : MonoBehaviour
 
     public RogueUpgrade Summary
     {
-        get {
+        get
+        {
             // Create a blank RogueUpgrade so we can assign stats
-            RogueUpgrade _summary = ScriptableObject.CreateInstance<RogueUpgrade>();
+            RogueUpgrade summary = ScriptableObject.CreateInstance<RogueUpgrade>();
+
+            List<RogueUpgrade> upgradesNoDupeUniques = new();
+
+            foreach (var upgrade in Upgrades)
+            {
+                if (upgrade.Unique)
+                {
+                    if (!upgradesNoDupeUniques.Contains(upgrade))
+                    {
+                        upgradesNoDupeUniques.Add(upgrade);
+                    }
+                }
+                else
+                {
+                    upgradesNoDupeUniques.Add(upgrade);
+                }
+            }
 
             // Upgrades is our list of upgrades (duh)
             // Upgrades.GetType() returns the type of the items in the list (RogueUpgrade)
             // Upgrades.GetType().GetProperties() returns the properties on the the type of the items in the list (RogueUpgrade)
-            foreach (var key in Upgrades.GetType().GetProperties() ) { 
+            foreach (var key in upgradesNoDupeUniques.GetType().GetProperties())
+            {
                 // Check if the value of it is an integer
-                if(key.PropertyType == typeof(int)) {
+                if (key.PropertyType == typeof(int))
+                {
                     // Upgrades.Select() lets you run a function for each member of a list (or other iterable) and returns the output of whatever function you ran
-                    
+
                     // The "=>" is called an arrow function and its just a handy shortcut 
                     // This could also be written as the following                    
                     // Upgrades.Select((upgrade) {
@@ -27,12 +46,13 @@ public class RogueUpgradeManager : MonoBehaviour
                     // }).Sum();
 
                     // key.ToString() is just a conversion from whatever weird type key.PropertyType spits out to a string, which we feed into the indexer function on RogueUpgrade
-                    Upgrades.Select((upgrade) => upgrade[key.ToString()]).Sum();
-                    
+                    summary[key.ToString()] = upgradesNoDupeUniques.Select((upgrade) => upgrade[key.ToString()]).Sum();
+
                 };
             };
 
-            return _summary;
+            return summary;
         }
     }
+
 }
