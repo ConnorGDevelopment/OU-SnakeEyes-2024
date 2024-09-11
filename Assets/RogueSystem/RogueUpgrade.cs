d0601330c2bc90ebff59da0129d7a54eaf07be32
ConnorGDevelopment <60874253+ConnorGDevelopment@users.noreply.github.com>
2024-09-11 13:47:54 -0400

    Switched to using Dictionary for upgrades

--- Assets/RogueSystem/RogueUpgrade.cs
+++ Assets/RogueSystem/RogueUpgrade.cs
@@ -3,32 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+// The following are TBD, should match weapon stats
+
+public enum RogueStat {
+    Damage,
+    Speed,
+    Projectiles,
+    Spread
+}
+
 public class RogueUpgrade: ScriptableObject
 {
+    public string Name { get; set; }
     // If Unique == true, duplicates of this upgrade don't stack
     public bool Unique { get; set; }
-    public string Name { get; set; }
-    // The following are TBD, should match weapon stats
-    public int Damage { get; set; }
-    public int Speed { get; set; }
-    public int Projectiles { get; set; }
-    public int Spread { get; set; }
-    // Indexer Function
-    // Basically assigns a function to this.propertynamegoeshere, if it exists and is an integer it returns that value, otherwise it returns 0
-    // This lets us index access properties on objects of this class as if it were an array like "this[propertynamegoeshere]"
-    public int this[string key]
-    {
-        get
-        {
-            if (this[key].GetType() == typeof(int))
-            {
-                return this[key];
-            }
-            else
-            {
-                // If this weren't a getter I could use int? which basically means this value is either int or undefined
-                return 0;
-            }
-        }
-    }
+    public Dictionary<RogueStat,int> Stats { get; set; }
 }
--- Assets/RogueSystem/RogueUpgradeManager.cs
+++ Assets/RogueSystem/RogueUpgradeManager.cs
@@ -1,55 +1,51 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public class RogueUpgradeManager : MonoBehaviour
 {
-    public readonly List<RogueUpgrade> Upgrades = new();
+    private List<RogueUpgrade> _upgrades = new();
+    public List<RogueUpgrade> Upgrades {
+        get { 
+            List<RogueUpgrade> _filteredUpgrades = new();
 
-    public RogueUpgrade Summary
-    {
-        get
-        {
-            // Create a blank RogueUpgrade so we can assign stats
-            RogueUpgrade summary = ScriptableObject.CreateInstance<RogueUpgrade>();
-
-            List<RogueUpgrade> upgradesNoDupeUniques = new();
-
-            foreach (var upgrade in Upgrades)
-            {
-                if (upgrade.Unique)
+            foreach (var upgrade in _upgrades) {
+                if(upgrade.Unique == false)
                 {
-                    if (!upgradesNoDupeUniques.Contains(upgrade))
+                    _filteredUpgrades.Add(upgrade);
+                } else
+                {
+                    if(_filteredUpgrades.Select((toMatch) => toMatch.Name).Contains(upgrade.Name) == false) 
                     {
-                        upgradesNoDupeUniques.Add(upgrade);
+                        _filteredUpgrades.Add(upgrade);
                     }
-                }
-                else
-                {
-                    upgradesNoDupeUniques.Add(upgrade);
                 }
             }
 
-            // Upgrades is our list of upgrades (duh)
-            // Upgrades.GetType() returns the type of the items in the list (RogueUpgrade)
-            // Upgrades.GetType().GetProperties() returns the properties on the the type of the items in the list (RogueUpgrade)
-            foreach (var key in upgradesNoDupeUniques.GetType().GetProperties())
+            return _filteredUpgrades;
+        }
+    }
+
+    public Dictionary<RogueStat,int> Summary
+    {
+        get
+        {
+            Dictionary<RogueStat, int> summary = new();
+
+
+            foreach (var upgrade in Upgrades)
             {
-                // Check if the value of it is an integer
-                if (key.PropertyType == typeof(int))
-                {
-                    // Upgrades.Select() lets you run a function for each member of a list (or other iterable) and returns the output of whatever function you ran
-
-                    // The "=>" is called an arrow function and its just a handy shortcut 
-                    // This could also be written as the following                    
-                    // Upgrades.Select((upgrade) {
-                    //  return upgrade[key.ToString()]
-                    // }).Sum();
-
-                    // key.ToString() is just a conversion from whatever weird type key.PropertyType spits out to a string, which we feed into the indexer function on RogueUpgrade
-                    summary[key.ToString()] = upgradesNoDupeUniques.Select((upgrade) => upgrade[key.ToString()]).Sum();
-
-                };
-            };
+                foreach (var stat in upgrade.Stats) {
+                    if(summary.ContainsKey(stat.Key))
+                    {
+                        summary[stat.Key] += upgrade.Stats[stat.Key];
+                    } else
+                    {
+                        summary.Add(stat.Key, stat.Value);
+                    }
+                }
+            }
 
             return summary;
         }
