fceb6ff056fb6504b1fbca644c7a48111a2c3807
Connor Guarino <connorgdevelopment@gmail.com>
2024-09-18 09:26:40 -0400

    Match to Eric + Cleanup

--- Assets/RogueSystem/RogueUpgrade.cs
+++ Assets/RogueSystem/RogueUpgrade.cs
@@ -1,34 +1,19 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class RogueUpgrade: ScriptableObject
+public class RogueUpgrade : ScriptableObject
 {
-    // If Unique == true, duplicates of this upgrade don't stack
-    public bool Unique { get; set; }
-    public string Name { get; set; }
-    // The following are TBD, should match weapon stats
-    public int Damage { get; set; }
-    public int Speed { get; set; }
-    public int Projectiles { get; set; }
-    public int Spread { get; set; }
-    // Indexer Function
-    // Basically assigns a function to this.propertynamegoeshere, if it exists and is an integer it returns that value, otherwise it returns 0
-    // This lets us index access properties on objects of this class as if it were an array like "this[propertynamegoeshere]"
-    public int this[string key]
+    public enum RogueStat
     {
-        get
-        {
-            if (this[key].GetType() == typeof(int))
-            {
-                return this[key];
-            }
-            else
-            {
-                // If this weren't a getter I could use int? which basically means this value is either int or undefined
-                return 0;
-            }
-        }
+        Damage,
+        Speed,
+        Projectiles,
+        Spread
     }
+
+    public string Name;
+    // If Unique == true, duplicates of this upgrade don't stack
+    public bool Unique;
+    // Readonly dict means elements can be added/removed but the dict can't be replaced
+    public readonly Dictionary<RogueStat, int> Stats = new();
 }
--- Assets/RogueSystem/RogueUpgradeManager.cs
+++ Assets/RogueSystem/RogueUpgradeManager.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
-using System.Linq;
+using UnityEngine;
 
 public class RogueUpgradeManager : MonoBehaviour
 {
+    // Readonly list means elements can be added/removed but the list can't be replaced
     public readonly List<RogueUpgrade> Upgrades = new();
 
     public RogueUpgrade Summary
@@ -12,44 +13,36 @@ public class RogueUpgradeManager : MonoBehaviour
             // Create a blank RogueUpgrade so we can assign stats
             RogueUpgrade summary = ScriptableObject.CreateInstance<RogueUpgrade>();
 
-            List<RogueUpgrade> upgradesNoDupeUniques = new();
+            // Start new list of upgrades to do some filtering
+            List<RogueUpgrade> upgrades = new();
 
             foreach (var upgrade in Upgrades)
             {
-                if (upgrade.Unique)
+                // If upgrade IS unique and there is not a copy added already
+                // If upgrade is not unique
+                if ((upgrade.Unique && !upgrades.Contains(upgrade)) || !upgrade.Unique)
                 {
-                    if (!upgradesNoDupeUniques.Contains(upgrade))
-                    {
-                        upgradesNoDupeUniques.Add(upgrade);
-                    }
-                }
-                else
-                {
-                    upgradesNoDupeUniques.Add(upgrade);
+                    upgrades.Add(upgrade);
                 }
             }
 
-            // Upgrades is our list of upgrades (duh)
-            // Upgrades.GetType() returns the type of the items in the list (RogueUpgrade)
-            // Upgrades.GetType().GetProperties() returns the properties on the the type of the items in the list (RogueUpgrade)
-            foreach (var key in upgradesNoDupeUniques.GetType().GetProperties())
+            // Go through each upgrade in filtered list of upgrades
+            foreach (var upgrade in upgrades)
             {
-                // Check if the value of it is an integer
-                if (key.PropertyType == typeof(int))
+                // Go through each stat
+                foreach (var stat in upgrade.Stats)
                 {
-                    // Upgrades.Select() lets you run a function for each member of a list (or other iterable) and returns the output of whatever function you ran
-
-                    // The "=>" is called an arrow function and its just a handy shortcut 
-                    // This could also be written as the following                    
-                    // Upgrades.Select((upgrade) {
-                    //  return upgrade[key.ToString()]
-                    // }).Sum();
-
-                    // key.ToString() is just a conversion from whatever weird type key.PropertyType spits out to a string, which we feed into the indexer function on RogueUpgrade
-                    summary[key.ToString()] = upgradesNoDupeUniques.Select((upgrade) => upgrade[key.ToString()]).Sum();
-
-                };
-            };
+                    // If stat exists, increment by value, otherwise add the stat and value
+                    if (summary.Stats.ContainsKey(stat.Key))
+                    {
+                        summary.Stats[stat.Key] += stat.Value;
+                    }
+                    else
+                    {
+                        summary.Stats.Add(stat.Key, stat.Value);
+                    }
+                }
+            }
 
             return summary;
         }
