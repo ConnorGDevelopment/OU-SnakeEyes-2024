b7f09b64393e61fb45e6109ec47bba16b17eba0d
EricPopp76 <thunderhawks76@gmail.com>
2024-10-09 12:52:54 -0400

    Holster start

--- Assets/Weapons/HolsterScript.cs
+++ Assets/Weapons/HolsterScript.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HolsterScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public GameObject Revolver;
+    public Transform HolsterLeft;
+    public Transform HolsterRight;
+    public BoxCollider GunCollider;
+
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("GunDestroy"))
+        {
+            if (Revolver.tag == "LeftRevolver")
+            {
+                GameObject OriginalRevolver = GameObject.FindWithTag("LeftRevolver");
+
+                Destroy(OriginalRevolver);
+
+                Instantiate(Revolver, HolsterLeft.position, HolsterLeft.rotation);
+
+            }
+
+
+
+
+            if (Revolver.tag == "RightRevolver")
+            {
+                GameObject OriginalRevolver = GameObject.FindWithTag("RightRevolver");
+                
+                Destroy(OriginalRevolver);
+
+                Instantiate(Revolver, HolsterRight.position, HolsterRight.rotation);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
