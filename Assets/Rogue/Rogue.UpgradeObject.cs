1c2ad08c496d070eb039ab5576d23bd53dec698c
EricPopp76 <thunderhawks76@gmail.com>
2024-11-08 20:32:33 -0500

    Finished wave management
    The upgrade now starts a new wave after the number of enemies have been destroyed

--- Assets/Rogue/Rogue.UpgradeObject.cs
+++ Assets/Rogue/Rogue.UpgradeObject.cs
@@ -8,20 +8,25 @@ namespace Rogue
     public class UpgradeObject : MonoBehaviour
     {
         public UpgradeData Upgrade;
-
+        
         
 
         private UpgradeManager _upgradeManager;
-
+        //private SpawningScript _spawningScript;
         public void Start()
         {
             _upgradeManager = UpgradeManager.FindLive(gameObject);
+            //_spawningScript = SpawningScript.FindLive(gameObject);
+
         }
 
         public void OnGrab(SelectEnterEventArgs ctx)
         {
             Debug.Log("Click");
             _upgradeManager.Upgrades.Add(Upgrade);
+            //_spawningScript.ResetEnemies();
+
+            
         }
     }
 
